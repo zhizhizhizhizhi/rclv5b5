d194eff5ad12ae6d1676c024e55c926072a080bb
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2022-09-14 22:33:04 +0800

    update configuration

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
+from os import environ 
 import subprocess
 
 final_db_url = 'postgresql://localhost/url_shortener'
-if os.environ['FLASK_ENV'] == 'production':
+if environ['FLASK_ENV'] == 'production':
    heroku_app_name = 'rclv5b5'
    raw_db_url = subprocess.run(
       ["heroku", "config:get", "DATABASE_URL", "--app", heroku_app_name],
@@ -12,7 +12,7 @@ if os.environ['FLASK_ENV'] == 'production':
    db_url = raw_db_url.decode("ascii").strip()
 
    final_db_url = "postgresql+psycopg2://" + db_url.lstrip("postgres://")
-
+print("config: " + final_db_url)
 # for localhost
 SQLALCHEMY_DATABASE_URI = final_db_url 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
