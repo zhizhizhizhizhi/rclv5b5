7950fea3cdbc378ce273d2e80876b76c43d88347
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2022-09-14 23:10:53 +0800

    update config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 from os import environ 
 
-SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL'].replace('postgresql') 
+SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL'].replace('postgres', 'postgresql') 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
