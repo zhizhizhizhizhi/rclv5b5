299bbbca267ff02a8d065f0686bdbe10fd2a6f63
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2022-09-14 20:26:09 +0800

    add references

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # url-shortener
  
+References:
+- [How To Make a URL Shortener with Flask and SQLite](https://www.digitalocean.com/community/tutorials/how-to-make-a-url-shortener-with-flask-and-sqlite)
+- [Create a Web App and Deploy it to the Cloud](https://arctype.com/blog/postgres-heroku/)
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+# SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+#     'postgresql://lqqiacheenotxq:1a9ff45fbb6030b7d9f431f281fc659a1d9f8a94f80a21ae79bcdf230c7220e4@ec2-34-200-205-45.compute-1.amazonaws.com:5432/dbm4asj0lps98v'
+#     # 'postgresql://localhost/url_shortener'
+
+# for localhost
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
-    'postgresql://lqqiacheenotxq:1a9ff45fbb6030b7d9f431f281fc659a1d9f8a94f80a21ae79bcdf230c7220e4@ec2-34-200-205-45.compute-1.amazonaws.com:5432/dbm4asj0lps98v'
-    # 'postgresql://localhost/url_shortener'
+   'postgresql://localhost/url_shortener'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
